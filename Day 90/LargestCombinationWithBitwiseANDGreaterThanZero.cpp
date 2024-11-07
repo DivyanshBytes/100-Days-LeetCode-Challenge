fc6306435733e07fd82455000d5619848a32b2b3
DivyanshBytes <shrivastav0885@gmail.com>
2024-11-07 11:57:06 +0530

    Day 90

---
+++ Day 90/LargestCombinationWithBitwiseANDGreaterThanZero.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int largestCombination(vector<int>& candidates) {
+        int MaxCount = 0;
+
+        for(int bit=0; bit<32; bit++){
+            int count = 0;
+            for(int num : candidates){
+                if (num & (1 << bit)) {
+                    count++;
+                }
+            }
+            MaxCount = max(MaxCount, count);
+        }
+        return MaxCount;
+    }
+};
