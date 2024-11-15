e68ec3b2831d99396045fed6bdc8ddb5fbe8ccb4
DivyanshBytes <shrivastav0885@gmail.com>
2024-11-15 16:53:32 +0530

    Day 98

---
+++ Day 98/ShortestSubarraytobeRemoved toMakeArraySorted.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int findLengthOfShortestSubarray(vector<int>& arr) {
+        int n = arr.size();
+        int right = n-1;
+
+        while (right > 0 && arr[right - 1] <= arr[right])
+            right--;
+        
+        if (right == 0)
+            return 0;
+        
+        int mini = right;
+        for (int left = 0; left < n; left++) {
+            if (left > 0 && arr[left - 1] > arr[left]) 
+                break;
+            
+            while (right < n && arr[left] > arr[right])
+                right++;
+            
+            int current = right - left - 1;
+            mini = min(mini, current);
+        }
+        return mini;
+    }
+};
